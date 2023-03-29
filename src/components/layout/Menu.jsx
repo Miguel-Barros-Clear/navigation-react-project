e63137abfdb8e921d60c86c2be11da59363db35b
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-29 09:04:01 -0300

    feat: creating application styles

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -2,5 +2,18 @@ import React from "react";
 import "./Menu.css";
 
 export default function Menu() {
-  return <aside className="Menu">Menu</aside>;
+  return (
+    <aside className="Menu">
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+          <li>
+            <a href="/">Sobre</a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
 }
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import Content from "../components/layout/Content";
+import Menu from "../components/layout/Menu";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Menu />
+      <Content />
+    </div>
+  );
 }
